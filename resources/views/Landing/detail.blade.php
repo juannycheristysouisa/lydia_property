e4181cc2364e10204ca638f0c41ba3fb302c0a0b
juannycheristysouisa <cheristysouisa@gmail.com>
2025-08-27 05:44:05 +0700

    Upload halaman

--- resources/views/Landing/detail.blade.php
+++ resources/views/Landing/detail.blade.php
@@ -343,6 +343,13 @@
     @endif
 </section>
 
+<!-- Footer -->
+    <footer>
+        <div class="container">
+            <p>&copy; 2025 Lydia Property. All rights reserved.</p>
+        </div>
+    </footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MRCrQYXXK+9v5GsmIZWnVdjOygU44zA6AJlC9Pj1sAgm+PZQjZ6drJwRkx6D6VLE"
         crossorigin="anonymous"></script>
