acec0007a0d2e811018b5b684f4daa49e44b0ba6
Cheristy Souisa <cheristy@gmail.com>
2025-11-05 15:03:49 +0700

    Update halaman

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -6,30 +6,23 @@ use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('Landing.index');
     }
 
-    // Halaman detail property
     public function detail($id)
-{
-    return view('Landing.detail', compact('id'));
-}
-    // Halaman daftar property
-    public function property()
     {
-        return view('Landing.property'); // nanti buat file property.blade.php
+        return view('Landing.detail', compact('id'));
     }
 
-    // Halaman contact
+    public function property()
+    {
+        return view('Landing.property');
+    }
+
     public function contact()
     {
-        return view('Landing.contact'); // nanti buat file contact.blade.php
+        return view('Landing.contact');
     }
 }
