8580b0db92272ac53a95ffd43a3ac3768f25fb66
Cheristy Souisa <cheristy@gmail.com>
2025-11-13 20:45:46 +0700

    updade halaman

--- resources/views/Landing/index.blade.php
+++ resources/views/Landing/index.blade.php
@@ -258,6 +258,24 @@
     </script>
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="{{ asset('js/script.js') }}"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const hamburger = document.getElementById("hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+
+        hamburger.addEventListener("click", () => {
+            navMenu.classList.toggle("active");
+            hamburger.classList.toggle("active");
+        });
+
+        document.querySelectorAll(".nav-link").forEach(link => {
+            link.addEventListener("click", () => {
+                navMenu.classList.remove("active");
+                hamburger.classList.remove("active");
+            });
+        });
+    });
+    </script>
 </body>
 
 </html>
